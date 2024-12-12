0a4bc39f4b31e8abcb3c0ba6fe7c1b0305261ece
Yt99a1 <142407843+Yt99a1@users.noreply.github.com>
2024-12-12 20:27:26 +0500

    updated traindistribution.cs file

--- TrainDistribution.cs
+++ TrainDistribution.cs
@@ -1,32 +1,27 @@
 ﻿using System;
-using initize_diet_size;
-using Animalfunction;
 using System.Collections.Generic;
+using System.Linq;
+using Animalfunction;
 using TrainFunction;
 
 namespace TrainDistFunction
 {
     public class TrainDistribution
     {
-        public List<Train> Trains { get; set; }
-
-        public TrainDistribution()
-        {
-            Trains = new List<Train>();
-        }
+        public List<Train> Trains { get; } = new List<Train>();
 
         public void AddAnimal(Animal animal)
         {
-            
-            foreach (var Train in Trains)
+
+            foreach (var train in Trains)
             {
-                if (Train.AddAnimal(animal))
+                if (train.AddAnimal(animal))
                 {
                     return;
                 }
             }
 
-            
+
             var newTrain = new Train();
             newTrain.AddAnimal(animal);
             Trains.Add(newTrain);
@@ -34,21 +29,10 @@ namespace TrainDistFunction
 
         public void DistributeAnimals(List<Animal> animals)
         {
-            
-            animals = animals.OrderByDescending(a => a.Size).ToList();
 
-            
-            var meatEaters = animals.Where(a => a.Diet == Diet.Meat).ToList();
-            var plantEaters = animals.Where(a => a.Diet == Diet.Plant).ToList();
+            animals = animals.OrderByDescending(a => a.GetAnimalSize).ToList();
 
-            
-            foreach (var animal in meatEaters)
-            {
-                AddAnimal(animal);
-            }
-
-          
-            foreach (var animal in plantEaters)
+            foreach (var animal in animals)
             {
                 AddAnimal(animal);
             }
